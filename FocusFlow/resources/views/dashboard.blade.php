5a27614306e5dc316325a00fc0fd501842fdc1ba
Pythonista <pythonista@localhost.localdomain>
2023-07-11 10:52:02 +0300

    side navigation

--- FocusFlow/resources/views/dashboard.blade.php
+++ FocusFlow/resources/views/dashboard.blade.php
@@ -45,12 +45,12 @@
             <div class="col-xl-5 ms-auto mt-xl-0 mt-4">
                 <div class="row">
                     <div class="col-12">
-                        <div class="card bg-gradient-dark ">
+                        <div class="card bg-gray-800">
                             <div class="card-body p-3">
                                 <div class="row">
                                     <div class="numbers">
                                         <p class="text-white text-sm mb-0 text-capitalize font-weight-bold opacity-7"></p>
-                                        <h6 class="text-white font-weight-bolder mb-0">
+                                        <h6 class="text-green font-weight-bolder mb-0">
                                             {{$today->format(config('app.date_time_format'))}}
                                         </h6>
                                     </div>
@@ -61,20 +61,20 @@
                     </div>
                 </div>
                 <div class="row mt-4">
-                    <div class="col-md-6">
-                        <div class="card">
+                    <div class="col-md-6 ">
+                        <div class="card bg-gray-800">
                             <div class="card-body text-center">
-                                <h1 class="text-gradient text-dark"><span id="status1" countto="21">{{$total_goals}}</span> </h1>
-                                <h6 class="mb-0 font-weight-bolder">{{__('Goals')}}</h6>
+                                <h1 class="text-white"><span id="status1" countto="21">{{$total_goals}}</span> </h1>
+                                <h6 class="mb-0 font-weight-bolder text-white">{{__('Goals')}}</h6>
 
                             </div>
                         </div>
                     </div>
                     <div class="col-md-6 mt-md-0 mt-4">
-                        <div class="card">
+                        <div class="card bg-gray-800">
                             <div class="card-body text-center">
-                                <h1 class="text-gradient text-dark"> <span id="status2" countto="44">{{$total_plans}}</span></h1>
-                                <h6 class="mb-0 font-weight-bolder">{{__('Plans')}}</h6>
+                                <h1 class="text-white"> <span id="status2" countto="44">{{$total_plans}}</span></h1>
+                                <h6 class="text-white mb-0 font-weight-bolder">{{__('Plans')}}</h6>
 
                             </div>
                         </div>
@@ -82,19 +82,19 @@
                 </div>
                 <div class="row mt-4">
                     <div class="col-md-6">
-                        <div class="card">
+                        <div class="card bg-gray-800">
                             <div class="card-body text-center">
-                                <h1 class="text-gradient text-dark"><span id="status3" countto="87">{{$total_notes}}</span></h1>
-                                <h6 class="mb-0 font-weight-bolder">{{__('Notes')}}</h6>
+                                <h1 class="text-white"><span id="status3" countto="87">{{$total_notes}}</span></h1>
+                                <h6 class="text-white mb-0 font-weight-bolder">{{__('Notes')}}</h6>
 
                             </div>
                         </div>
                     </div>
                     <div class="col-md-6 mt-md-0 mt-4">
-                        <div class="card">
+                        <div class="card bg-gray-800">
                             <div class="card-body text-center">
-                                <h1 class="text-gradient text-dark"><span id="status4" countto="417">{{$total_projects}}</span></h1>
-                                <h6 class="mb-0 font-weight-bolder">{{__('Projects')}}</h6>
+                                <h1 class="text-white"><span id="status4" countto="417">{{$total_projects}}</span></h1>
+                                <h6 class="text-white mb-0 font-weight-bolder">{{__('Projects')}}</h6>
 
                             </div>
                         </div>
@@ -103,10 +103,10 @@
             </div>
         </div>
 
-        <div class="card mt-4">
-            <div class="card-header pb-0 p-3">
-                <div class="d-flex align-items-center">
-                    <h6 class="mb-0">{{__('Recent Projects')}}</h6>
+        <div class="card bg-gray-800 mt-4">
+            <div class="card-header bg-gray-800 pb-0 p-3">
+                <div class="d-flex  align-items-center">
+                    <h6 class="text-white font-weight-bold mb-0">{{__('Recent Projects')}}</h6>
 
                 </div>
             </div>
@@ -114,16 +114,16 @@
                 <table class="table align-items-center mb-0">
                     <thead>
                     <tr>
-                        <th class="text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
+                        <th class="text-uppercase text-white text-xs font-weight-bolder opacity-7">
                             {{__('Project Name')}}
 
                         </th>
-                        <th class="text-uppercase  text-secondary text-center text-xs font-weight-bolder opacity-7">
+                        <th class="text-uppercase  text-white text-center text-xs font-weight-bolder opacity-7">
                             {{__('Start Date')}}
 
                         </th>
 
-                        <th class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">{{__('End Date')}}</th>
+                        <th class="text-center text-uppercase text-white text-xs font-weight-bolder opacity-7">{{__('End Date')}}</th>
                         <th></th>
 
                     </tr>
@@ -138,7 +138,7 @@
                                         <h6 class="text-white">{{$project->title['0']}}</h6>
                                     </div>
                                     <a href="/view-project?id={{$project->id}}" class="d-flex flex-column justify-content-center">
-                                        <h6 class="mb-0 text-sm">{{$project->title}}</h6>
+                                        <h6 class="text-white mb-0 text-sm">{{$project->title}}</h6>
 
 
                                     </a>
@@ -146,12 +146,12 @@
                             </td>
 
                             <td class="align-middle text-center text-sm">
-                                <span class="badge bg-gradient-success font-weight-bold">@if(!empty($project->start_date))
+                                <span class="badge bg-blue font-weight-bold">@if(!empty($project->start_date))
                                         {{$project->start_date->format(config('app.date_format'))}}
                                     @endif</span>
                             </td>
                             <td class="align-middle text-center text-sm">
-                                <span class="badge bg-gradient-primary font-weight-bold">@if(!empty($project->end_date))
+                                <span class="badge bg-red font-weight-bold">@if(!empty($project->end_date))
                                         {{$project->end_date->format(config('app.date_format'))}}
                                     @endif</span>
                             </td>
@@ -174,32 +174,32 @@
         </div>
         <div class="row mt-4">
             <div class="col-lg-5 ms-auto">
-                <div class="card">
-                    <div class="card-header pb-0 p-3">
+                <div class="card bg-gray-800">
+                    <div class="card-header bg-gray-800 pb-0 p-3">
                         <div class="d-flex align-items-center">
-                            <h6 class="mb-0">{{__('Recent Events')}}</h6>
+                            <h6 class="text-white mb-0">{{__('Recent Events')}}</h6>
 
                         </div>
                     </div>
 
                     <div class="card">
 
-                        <div class="card-body border-radius-lg p-3">
+                        <div class="card-body bg-gray-800 border-radius-lg p-3">
 
                             @foreach($recent_events as $event)
 
                                 <div class="d-flex mt-4">
                                     <div>
-                                        <div class="icon icon-shape bg-gradient-faded-light shadow text-center border-radius-md shadow-none">
-                                            <i class="ni ni-bell-55 text-lg text-secondary  opacity-10" aria-hidden="true"></i>
+                                        <div class="icon icon-shape bg-green shadow text-center border-radius-md shadow-none">
+                                            <i class="ni ni-bell-55 text-lg text-dark  opacity-10" aria-hidden="true"></i>
                                         </div>
                                     </div>
                                     <div class="ms-3">
                                         <div class="numbers">
-                                            <h6 class="mb-1 text-dark text-sm">{{$event->title}}</h6>
+                                            <h6 class="mb-1 text-white text-sm">{{$event->title}}</h6>
 
-                                            <span class="text-sm">{{$event->start_date->format(config('app.date_time_format'))}}---
-                                                <span class="text-sm">{{$event->end_date->format(config('app.date_time_format'))}}</span></span>
+                                            <span class="text-sm text-white opacity-7">{{$event->start_date->format(config('app.date_time_format'))}}---
+                                                <span class="text-sm text-white opacity-7">{{$event->end_date->format(config('app.date_time_format'))}}</span></span>
 
 
 
@@ -222,17 +222,17 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="card mb-4">
-                            <div class="card-header pb-0">
-                                <h6>{{__('Goals')}}</h6>
+                            <div class="card-header bg-gray-800 pb-0">
+                                <h6 class="text-white">{{__('Goals')}}</h6>
                             </div>
-                            <div class="card-body px-0 pt-0 pb-2">
+                            <div class="card-body bg-gray-800 px-0 pt-0 pb-2">
                                 <div class="table-responsive p-0">
                                     <table class="table align-items-center mb-0">
                                         <thead>
                                         <tr>
-                                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">{{__('Name')}}</th>
-                                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">{{__('Estimate Date to finish')}}</th>
-                                            <th class="text-center float-end text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">{{__('Completed?')}}</th>
+                                            <th class="text-uppercase text-white text-xxs font-weight-bolder opacity-7">{{__('Name')}}</th>
+                                            <th class="text-uppercase text-white text-xxs font-weight-bolder opacity-7 ps-2">{{__('Estimate Date to finish')}}</th>
+                                            <th class="text-center float-end text-uppercase text-white text-xxs font-weight-bolder opacity-7">{{__('Completed?')}}</th>
 
 
 
@@ -242,21 +242,21 @@
                                         @foreach($goals as $goal)
                                             <tr>
                                                 <td>
-                                                    <div class="d-flex px-2 py-1">
+                                                    <div class="text-white d-flex px-2 py-1">
 
-                                                        <div class="d-flex flex-column justify-content-center">
-                                                            <h6 class="mb-0 text-sm"><a href="/set-goal?id={{$goal->id}}">{{$goal->name}}</a></h6>
+                                                        <div class="d-flex text-white flex-column justify-content-center">
+                                                            <h6 class="text-green mb-0 text-sm"><a href="/set-goal?id={{$goal->id}}">{{$goal->name}}</a></h6>
 
                                                         </div>
                                                     </div>
                                                 </td>
                                                 <td>
 
-                                                    <span class="text-xs font-weight-bold">{{$goal->date->format(config('app.date_format'))}}</span>
+                                                    <span class="text-xs text-white font-weight-bold opacity-7">{{$goal->date->format(config('app.date_format'))}}</span>
 
                                                 </td>
 
-                                                <td class="align-right text-sm">
+                                                <td class="align-right text-white text-sm">
                                                     <div class="form-check float-end ">
                                                         <input class="form-check-input goal_checkbox" type="checkbox"
                                                                data-id="{{$goal->id}}"
